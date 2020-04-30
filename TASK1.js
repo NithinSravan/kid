692ea6deae2a7a5b1ae75cebb317b2632fe81286
NithinSravan <56395258+NithinSravan@users.noreply.github.com>
2020-05-01 01:09:11 +0530

    Add files via upload

--- TASK1.js
+++ TASK1.js
@@ -1,36 +1,39 @@
+  
 //CREATING ARRAY AND RANDOMIZING IT
 var grid_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-function randomize(myarr) {      
-    var l = myarr.length;
+function randomize() {      
+    var l = grid_array.length;
     var temp;
     var index;
     while (l > 0) {  
        index = Math.floor(Math.random() * l);  
        l--;  
-       temp = myarr[l];          
-       myarr[l] = myarr[index];          
-       myarr[index] = temp;
+       temp = grid_array[l];          
+       grid_array[l] = grid_array[index];          
+       grid_array[index] = temp;
     }
-    return myarr;
+  
 }
- var newarr = randomize(grid_array);
-
+randomize();
+console.log(grid_array);
 //ASSIGNING
 var blocks = document.getElementsByClassName('box');
 var assign = function(){
-    for(i=0; i<20; i++){
-        blocks[i].innerHTML = newarr[i];
+    for(let i=0; i<20; i++){
+        blocks[i].innerHTML = grid_array[i];
     }
-    return blocks;
+    numclick();
 }
-assign(newarr);
+assign(grid_array);
 
 //NEWGAME BUTTON
-function resetbutton() {
-    document.getElementById("newgameid").reset();
-  }
 
-/*CLICKING A NUMBER
-funtion numclick(){
-    ​  document.getElementsByClassName("box").innerHTML = "+=20";
-}    */
+
+function numclick(i){
+    for(let i=0; i<20; i++){
+        blocks[i].addEventListener('click',function()
+        {
+            blocks[i].innerHTML=20+grid_array[i];
+        })
+    }      
+}    
